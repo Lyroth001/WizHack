54bd647aaadf1a5e0a6a4cebfc05185070ec59dc
Lye <zac@doodle.co.uk>
2023-11-26 00:32:32 +0000

    basic csv and player stats

--- WizHack/Assets/Assets/Scripts/controllerScript.cs
+++ WizHack/Assets/Assets/Scripts/controllerScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -11,11 +13,40 @@ public class controllerScript : MonoBehaviour
     // Start is called before the first frame update
     public int currentMap = 0;
     public Tilemap tileMapWalls;
-
+    public Dictionary<char,List<object>> monsters = new Dictionary<char, List<object>>();
     void Start()
     {
-       
-    }
+        //read all csvs
+        //read monster csv
+        using(var reader = new StreamReader(@"Assets\Assets\Materials\monsters.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var items = line.Split(',');
+                char key = 'a';
+                for(int i = 0; i < 6; i++)
+                {
+                    if(i==0)
+                    {
+                        key = items[i].ToCharArray()[0];
+                        monsters[key] = new List<object>();
+                        monsters[key].Add(items[i]);
+                    }
+                    else
+                    {
+                        monsters[key].Add(items[i]);
+                    }
+
+                }                
+            }
+            for(int i = 0; i < 6; i++)
+                {
+                    Debug.Log(monsters['a'][i]);
+                }
+                
+        }
+        }
 
     // Update is called once per frame
     void Update()
